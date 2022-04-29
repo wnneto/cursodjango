54ec8cd1910651013d76c68d65a492bd80346f46
wneto <n88131251>
2022-04-29 10:24:39 -0300

    Landing page

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -1,9 +1,21 @@
+import pytest
 from django.test import Client
+from django.urls import reverse
+
+from pypro.django_assertions import assert_contains
 
 
+@pytest.fixture
+def resp(client):
+    resp = Client.get(reverse('base:home'))
+    return resp
 
-def test_status_code(client: Client):
-    resp = client.get('/')
+def test_status_code(resp):
     assert resp.status_code == 200
 
+def test_title(resp):
+    assert_contains(resp, '<title>Karol BanK</title>')
+
+def test_home_link(resp):
+    assert_contains(resp, f'href="{reverse("base:home")}">Karol BanK</a>')
 
--- pypro/base/views.py
+++ pypro/base/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Boa noite, MEU AMOR!')
+    return render(request, 'base/home.html')
